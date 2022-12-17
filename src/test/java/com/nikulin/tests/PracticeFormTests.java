31a8e8dcb01e1e455d529987ba8e1690299b5270
Sergei Nikulin <nikulin-91@mail.ru>
2022-12-17 12:56:41 +0400

    add auto generation value variables

--- src/test/java/com/nikulin/tests/PracticeFormTests.java
+++ src/test/java/com/nikulin/tests/PracticeFormTests.java
@@ -1,5 +1,6 @@
 package com.nikulin.tests;
 
+import com.github.javafaker.Faker;
 import com.nikulin.pages.RegistrationPage;
 import org.junit.jupiter.api.Test;
 
@@ -8,19 +9,20 @@ import static com.nikulin.tests.TestData.*;
 public class PracticeFormTests extends TestBase {
 
     RegistrationPage registrationPage = new RegistrationPage();
+    Faker faker = new Faker();
 
     @Test
     public void practiceForm() {
-        String firstName = "Petr";
-        String lastName = "Petrov";
-        String email = "petrov@test.com";
+        String firstName = faker.name().firstName();
+        String lastName = faker.name().lastName();
+        String email = faker.internet().emailAddress();
         String dayOfBirth = "30";
         String monthOfBirth = "July";
         String yearOfBirth = "1992";
-        String userNumber = "7912345678";
+        String userNumber = faker.phoneNumber().subscriberNumber(10);
         String subjects = "Maths"; // Значение должно быть из доступного списка
         String namePicture = "testPicture1.png";
-        String currentAddress = "city: Testes. street: Homework" ;
+        String currentAddress = faker.address().fullAddress();
         String state = "Haryana"; // Допустимы только значения из справочника
         String city = "Karnal"; // Допустимы только значения из справочника
 
