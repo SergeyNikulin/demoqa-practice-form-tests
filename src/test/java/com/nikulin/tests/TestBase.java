19cf452e57e2ecbaca840b34f40203523d5cc995
Sergei Nikulin <nikulin-91@mail.ru>
2023-02-11 19:37:47 +0400

    edit configuration browser

--- src/test/java/com/nikulin/tests/TestBase.java
+++ src/test/java/com/nikulin/tests/TestBase.java
@@ -23,7 +23,7 @@ public class TestBase {
         Configuration.browserCapabilities = capabilities;
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "500x1000"; // Если есть проблема с отсутствием кнопки Submit, то необходимо подобрать разрешение при котором кнопка будет отображаться
-        //Configuration.browser = System.getProperty("browser");
+        Configuration.browser = System.getProperty("browser");
         //Configuration.browserVersion = System.getProperty("version");
         //Configuration.browserSize = System.getProperty("size"); // Если есть проблема с отсутствием кнопки Submit, то необходимо подобрать разрешение при котором кнопка будет отображаться
         Configuration.remote = getSelenoidUrl();
