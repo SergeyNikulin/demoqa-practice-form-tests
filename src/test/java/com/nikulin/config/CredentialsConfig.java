d98098b323fbc78cbf021e76d3f77accb5ad5c4e
Sergei Nikulin <nikulin-91@mail.ru>
2023-02-11 19:31:32 +0400

    refactoring

--- src/test/java/com/nikulin/config/CredentialsConfig.java
+++ src/test/java/com/nikulin/config/CredentialsConfig.java
@@ -2,6 +2,7 @@ package com.nikulin.config;
 
 import org.aeonbits.owner.Config;
 
+@Config.Sources({"classpath:config/credentials.properties"})
 public interface CredentialsConfig extends Config {
     String login();
     String password();
